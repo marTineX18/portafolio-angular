376ac44a2ebd9d9ab537ebc04ab15f93a0d0ca27
Gabriel Martinez <martinegabriel47@gmail.com>
2022-02-10 15:32:42 -0500

    Template listo sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shader/header/header.component';
+import { FooterComponent } from './shader/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
