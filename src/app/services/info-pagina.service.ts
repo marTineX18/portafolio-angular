d1e6acb3efff679f3fce8efdd7641434dcd695f6
Gabriel Martínez <martinegabriel47@gmail.com>
2022-02-23 15:22:07 -0500

    fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,22 +9,41 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) { 
-
-    // console.log('Servicio de infoPagina listo');
-
-    // leer el archivo JSON
-
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-
-          this.cargada = true;
-          this.info = resp;
-          console.log( resp );
-          
-
-        } );
+    this.cargarInfo();
+    this.cargarEquipo();
 
   }
+
+  private cargarInfo() {
+     // leer el archivo JSON
+
+     this.http.get('assets/data/data-pagina.json')
+     .subscribe( (resp: InfoPagina) => {
+       this.cargada = true;
+       this.info = resp;
+
+     } );
+  }
+
+
+  private cargarEquipo() {
+     // leer el archivo JSON
+
+     this.http.get('https://angular-html-78790-default-rtdb.firebaseio.com/.json')
+     .subscribe( (resp: any) => {
+
+       
+       this.equipo = resp;
+       console.log( resp );
+
+     } );
+
+    
+
+  }
+
 }
